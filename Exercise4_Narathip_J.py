6832a055abb84fde6715dd523f33037889609c6e
NarathipDev <narathip_aot@hotmail.com>
2021-09-04 21:23:26 +0700

    Added Exercise 4

---
+++ Exercise4_Narathip_J.py
@@ -0,0 +1,10 @@
+Foundation_English = 70.0
+General_Business = 80.5
+Introduction_to_Computer_Systems = 95.0
+Computer_Programming = 99.0
+
+print("---Your Score---")
+print("Foundation English :" ,Foundation_English)
+print("General Business :" ,General_Business)
+print("Introduction to Computer Systems :" ,Introduction_to_Computer_Systems)
+print("Computer Programming :" ,Computer_Programming)
