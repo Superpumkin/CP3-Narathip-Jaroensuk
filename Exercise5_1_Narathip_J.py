64c93be203a96442c29f3050c210934ac33377df
NarathipDev <narathip_aot@hotmail.com>
2021-09-06 20:51:31 +0700

    Create Exercise5_1_Narathip_J.py

---
+++ Exercise5_1_Narathip_J.py
@@ -0,0 +1,9 @@
+# Exercise5_1
+
+a = float(input("Number1 :"))
+b = float(input("Number2 :"))
+
+print(a , "+" , b , "=" ,a+b)
+print(a , "-" , b , "=" ,a-b)
+print(a , "*" , b , "=" ,a*b)
+print(a , "/" , b , "=" ,a/b)
