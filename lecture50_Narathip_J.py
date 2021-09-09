c1488867f6ea88285948ad6d03873076dfb77340
NarathipDev <narathip_aot@hotmail.com>
2021-09-09 21:09:19 +0700

    Create lecture50_Narathip_J.py

---
+++ lecture50_Narathip_J.py
@@ -0,0 +1,15 @@
+def add_Number(x,y) :
+    print(x+y)
+def subtract_Number(x,y) :
+    print(x-y)
+def multiply_Number(x,y) :
+    print(x*y)
+def divide_Number(x,y) :
+    print(x/y)
+
+x=int(input("Please Enter number of x :"))
+y=int(input("Please Enter number of y :"))
+add_Number(x,y)
+subtract_Number(x,y)
+multiply_Number(x,y)
+divide_Number(x,y)
